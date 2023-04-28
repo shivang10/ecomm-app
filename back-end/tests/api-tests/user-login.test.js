a66adaff51e46abb4f5ddd5c41e273a0fec3431d
Shivang Garg <shivang10garg@gmail.com>
2023-04-28 23:33:47 +0530

    adds test cases for user login BE

---
+++ back-end/tests/api-tests/user-login.test.js
@@ -0,0 +1,81 @@
+const dbHandler = require('../db-setup');
+const app = require("../../index");
+const request = require("supertest");
+const User = require("../../models/user");
+const saltRounds = 10;
+const bcrypt = require("bcryptjs");
+
+const {username, phoneNumber, password, email, wrongEmail, wrongPassword} = require("../dummy-data/user-dummy-data");
+
+beforeAll(async () => {
+    await dbHandler.connect()
+
+    const password = await bcrypt.hash("password", saltRounds);
+    const user = new User({
+        email, password, username, phoneNumber
+    });
+
+    await user.save();
+});
+
+afterEach(async () => await dbHandler.clearDatabase());
+
+afterAll(async () => await dbHandler.closeDatabase());
+
+
+describe("User Login", () => {
+    it("should successfully login user", async () => {
+        const userData = {
+            email, password
+        }
+
+        const res = await request(app).post("/user-login").send(userData);
+        const resData = JSON.parse(res["text"])
+        expect(res.status).toEqual(200);
+        expect(resData.message).toEqual("Successfully logged in");
+    });
+
+    it("error for wrong email", async () => {
+        const userData = {
+            email: wrongEmail, password
+        }
+
+        const res = await request(app).post("/user-login").send(userData);
+        const resData = JSON.parse(res["text"])
+        expect(res.status).toEqual(400);
+        expect(resData.message).toEqual("Wrong email or password. Try Again");
+    });
+
+    it("error for wrong password", async () => {
+        const userData = {
+            email, password: wrongPassword
+        }
+
+        const res = await request(app).post("/user-login").send(userData);
+        const resData = JSON.parse(res["text"]);
+        expect(res.status).toEqual(400);
+        expect(resData.message).toEqual("Wrong email or password. Try Again");
+    });
+
+    it("error for email missing", async () => {
+        const userData = {
+            password
+        }
+
+        const res = await request(app).post("/user-login").send(userData);
+        const resData = JSON.parse(res["text"]);
+        expect(res.status).toEqual(400);
+        expect(resData.message).toEqual("Email and password both are required.");
+    });
+
+    it("error for password missing", async () => {
+        const userData = {
+            email
+        }
+
+        const res = await request(app).post("/user-login").send(userData);
+        const resData = JSON.parse(res["text"]);
+        expect(res.status).toEqual(400);
+        expect(resData.message).toEqual("Email and password both are required.");
+    });
+});
--- back-end/tests/dummy-data/user-dummy-data.js
+++ back-end/tests/dummy-data/user-dummy-data.js
@@ -3,5 +3,6 @@ const email = "email@email.com";
 const password = "password";
 const phoneNumber = "79679343";
 const wrongEmail = "abc@def";
+const wrongPassword = "wrong";
 
-module.exports = {username, email, password, phoneNumber, wrongEmail};
+module.exports = {username, email, password, phoneNumber, wrongEmail, wrongPassword};
