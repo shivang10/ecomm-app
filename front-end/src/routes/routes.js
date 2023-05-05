a3afec0a2c78cef8bc5221a3326ba3f01b2edc33
Shivang Garg <shivang10garg@gmail.com>
2023-05-05 22:36:33 +0530

    implements lazy loading FE

--- front-end/src/routes/routes.js
+++ front-end/src/routes/routes.js
@@ -1,15 +1,20 @@
+import {lazy, Suspense} from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {homepageLink, userLoginLink, userRegisterLink} from "./routesLink";
-import Homepage from "../homepage/homepage";
-import UserRegister from "../user-auth/user-register";
-import UserLogin from "../user-auth/user-login";
+
+const Homepage = lazy(() => import("../homepage/homepage"));
+const UserRegister = lazy(() => import("../user-auth/user-register"));
+const UserLogin = lazy(() => import("../user-auth/user-login"));
 
 const PageRoutes = () => {
     return (
-        <Routes>
-            <Route exact path={homepageLink} element={<Homepage />}/>
-            <Route exact path={userLoginLink} element={<UserLogin />}/>
-            <Route exact path={userRegisterLink} element={<UserRegister />}/>
-        </Routes>)
+        <Suspense fallback={<div>...</div>}>
+            <Routes>
+                <Route exact path={homepageLink} element={<Homepage/>}/>
+                <Route exact path={userLoginLink} element={<UserLogin/>}/>
+                <Route exact path={userRegisterLink} element={<UserRegister/>}/>
+            </Routes>
+        </Suspense>
+    )
 }
 export default PageRoutes;
