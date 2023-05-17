2d075e64d299fcccc48673a626bef191b0011180
Shivang Garg <shivang10garg@gmail.com>
2023-05-17 22:39:52 +0530

    adds api response tests

---
+++ back-end/tests/utils-tests/api-response.test.js
@@ -0,0 +1,33 @@
+const apiResponse = require("../../utils/api-response");
+
+const message = "message";
+const data = "data";
+
+describe("tests for api response", () => {
+    it("should return response with message and data", () => {
+        const completeResult = {
+            message, data
+        }
+
+        const response = apiResponse(data, message);
+        expect(response).toEqual(completeResult);
+    });
+
+    it("should return response with message only", () => {
+        const messageResult = {
+            message, data: null
+        }
+
+        const response = apiResponse(null, message);
+        expect(response).toEqual(messageResult);
+    });
+
+    it("should return response with data only", () => {
+        const dataResult = {
+            data, message: ""
+        }
+
+        const response = apiResponse(data);
+        expect(response).toEqual(dataResult);
+    });
+});
