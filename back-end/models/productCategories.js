8d1c681a97efde8eeb0a42e87dc4a8c4c4cb1ecf
Shivang Garg <shivang10garg@gmail.com>
2023-05-28 15:47:26 +0530

    adds product categories model

---
+++ back-end/models/productCategories.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const ProductCategories = new mongoose.Schema({
+    category: {type: String, required: true, unique: true}
+});
+
+const model = mongoose.model("ProductCategories", ProductCategories);
+module.exports = model;
