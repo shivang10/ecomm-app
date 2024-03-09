61ca3795cbfd245dc6277235745853fb3f6659eb
Shivang Garg <shivang10garg@gmail.com>
2024-03-09 18:15:54 +0530

    fixes get all products list

--- back-end/routes/common/get-products-list.js
+++ back-end/routes/common/get-products-list.js
@@ -6,10 +6,11 @@ const ProductSchema = require("../../models/products");
 
 router.get("/", async (req, res) => {
     const maxProductsAllowedToBeFetched = 100;
-    const limit = Math.min(req.query.limit, maxProductsAllowedToBeFetched) || 10;
-    const page = req.query.page || 1;
+    const limit = req.query.limit || maxProductsAllowedToBeFetched;
+    const page = req.query.page || 0;
+    const skipDocs = page * limit;
     try {
-        const productsList = await ProductSchema.find().skip(page).limit(limit);
+        const productsList = await ProductSchema.find({}).skip(skipDocs).limit(limit);
         return res.status(200).send(apiResponse(productsList, "Products are successfully fetched."));
     } catch (error) {
         return res.status(500).send(apiResponse(error, "Unable to fetch products. Please try after sometime."));
