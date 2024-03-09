a8eb1edca1bddf79f0c953b3c83f80aa21e68407
Shivang Garg <shivang10garg@gmail.com>
2024-03-09 17:18:08 +0530

    fixes fetch all user past orders

--- back-end/routes/users/orders/get-user-orders.js
+++ back-end/routes/users/orders/get-user-orders.js
@@ -4,20 +4,18 @@ const router = express.Router();
 const apiResponse = require("../../../utils/api-response");
 const validateUserToken = require("../../../middlewares/validate-user-token");
 const OrdersSchema = require("../../../models/orders");
+const generateObjectId = require("../../../utils/generate-object-id");
 
 router.get("/:id", validateUserToken, async (req, res) => {
-    const userId = req.params.id;
-
-    if (!userId) {
-        return res.status(400).send(apiResponse(null, "UserId is required"));
-    }
+    const userId = generateObjectId(req.params.id);
 
     try {
         const maxProductsAllowedToBeFetched = 20;
-        const limit = Math.min(req.query.limit, maxProductsAllowedToBeFetched) || 10;
-        const page = req.query.page || 1;
-        const paymentMethods = await OrdersSchema.find({ userId }).skip(page).limit(limit);
-        return res.status(200).send(apiResponse(paymentMethods, "Orders are successfully fetched."));
+        const limit = req.query.limit || maxProductsAllowedToBeFetched;
+        const page = req.query.page || 0;
+        const skipDocs = page * limit;
+        const pastOrders = await OrdersSchema.find({ userId }).skip(skipDocs).limit(limit);
+        return res.status(200).send(apiResponse(pastOrders, "Orders are successfully fetched."));
     } catch (error) {
         return res.status(500).send(apiResponse(error, "Unable to fetch user orders."));
     }
