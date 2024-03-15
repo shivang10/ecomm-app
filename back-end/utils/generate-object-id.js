8c2906cb79a356f8ba488c08bd1c4ac624b96523
Shivang Garg <shivang10garg@gmail.com>
2024-03-15 18:38:00 +0530

    puts generate object in try catch

--- back-end/utils/generate-object-id.js
+++ back-end/utils/generate-object-id.js
@@ -1,5 +1,12 @@
 const { Types } = require("mongoose");
+const apiResponse = require("./api-response");
 
-const generateObjectId = (id) => new Types.ObjectId(id);
+const generateObjectId = (id) => {
+    try {
+        return new Types.ObjectId(id);
+    } catch (error) {
+        return apiResponse(error, "cannot convert to objectId");
+    }
+};
 
 module.exports = generateObjectId;
